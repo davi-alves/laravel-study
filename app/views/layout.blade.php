b8f4edae206d39b7efd1b3143ead9ce871bce79a
Davi Alves <flyingkrai@hotmail.com>
2013-05-28 23:53:53 -0300

    project

---
+++ app/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Study</title>
+    <link rel="stylesheet" href="{{ url('bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ url('bootstrap/css/bootstrap-responsive.min.css')}}">
+</head>
+<body>
+     @yield('content')
+</body>
+</html>
